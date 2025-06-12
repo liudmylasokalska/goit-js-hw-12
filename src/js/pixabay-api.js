bc8388cb2d1cb3d7fdd1c8b3204fbdd8d8bec1c8
LS <ludddka@gmail.com>
2025-06-12 07:22:53 +0200

    js12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,22 +1,23 @@
 import axios from 'axios';
 
-const myKey = "50802626-049eec3b85d267f80d4405b2d";
+const myKey = "50629766-ca4e4f15305d377d5e3228fef";
 const url = 'https://pixabay.com/api/';
 
-export function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1, perPage = 15) {
   const params = {
     key: myKey,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
-    safesearch: true
+    safesearch: true,
+    page,
+    per_page: perPage,
   };
 
-  return axios.get(url, { params })
-    .then(response => {
-      if (response.data.hits.length === 0) {
-        throw new Error('Sorry, there are no images matching your search query. Please try again!');
-      }
-      return response.data; 
-    });
-}
+  try {
+    const response = await axios.get(url, { params })
+    return response.data; 
+  } catch(error) {
+    throw new Error('Sorry, there are no images matching your search query. Please try again!');
+  }
+};
